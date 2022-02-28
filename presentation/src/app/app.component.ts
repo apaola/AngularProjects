97e140349724c019de4a8aa6ab834489dec46d06
Alba Paola de la Cruz Solano <apaola484@gmail.com>
2022-02-28 03:18:04 -0400

    listo

---
+++ presentation/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Alba Paola';
+  lastname = 'de la Cruz Solano';
+  bio = 'Ingeniera de Sistema egresada de la Universidad Central del Este (UCE) y del Tecnólogo de Desarrollo de Software del Instituto Tecnológico de las Américas (ITLA).';
+}
